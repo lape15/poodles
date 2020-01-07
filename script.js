4d53d565f6b453fec5bc7749b0a3e2e1a549e016
Lape15 <larape1993@gmail.com>
2020-01-07 18:52:42 +0100

    update

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ fetch(DOG_URL)
     function handleChange(e) {
         
         loader.classList.add = ('show')
-        mainImg.style.display = ('show');
+        mainImg.style.display = 'none';
         const dog = e.target.value;
         const url = (`https://dog.ceo/api/breed/${dog}/images/random`)
 
@@ -38,5 +38,5 @@ fetch(DOG_URL)
 
     mainImg.addEventListener('load', function() {
         loader.classList.remove = ('show');
-        mainImg.style.display = 'block'
+        mainImg.classList.add = ('show');
        });
